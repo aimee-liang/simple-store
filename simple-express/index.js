3b5597afcd2bcd1c5c045df5d53569b1ff3c9159
aimee-liang <aimeeeliang@gmail.com>
2021-01-31 21:05:03 -0500

    Working on a simple express app

--- simple-express/index.js
+++ simple-express/index.js
@@ -1,4 +1,11 @@
 const express = require ('express')
 const app = express()
-const port = 3000
+const port = 8000
 
+app.get('/', (request, response) => {
+    response.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
