e4a8c04e04c34437acfe9653ebaed1dbd3283304
aimee-liang <aimeeeliang@gmail.com>
2021-02-04 16:15:35 -0500

    Adding details to Cart.tsx

---
+++ simple-store/src/Cart/Cart.tsx
@@ -0,0 +1,18 @@
+import CartItem from "../CartItem/CartItem"
+import {Wrapper} from "./Cart.styles"
+import {CartItemType} from "../App"
+
+type Props = {
+    cartItems: CartItemType[];
+    addToCart: (clickedItem: CartItemType) => void;
+    removeFromCart: (id: number) => void
+}
+
+const Cart: React.FC<Props> = ({cartItems, addToCart, removeFromCart}) => {
+    return (
+        <Wrapper>
+            <h2>Your Shopping Cart</h2>
+            {cartItems.length === 0 ? <p>"Your cart is empty"</p> : null}
+        </Wrapper>
+    )
+}
