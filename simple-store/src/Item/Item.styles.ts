c20a978f57efff49e3efeac5d550dc82a2ab7bd2
aimee-liang <aimeeeliang@gmail.com>
2021-02-03 21:25:47 -0500

    Need to debug

--- simple-store/src/Item/Item.styles.ts
+++ simple-store/src/Item/Item.styles.ts
@@ -1,3 +1,16 @@
 import styled from "styled-components"
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    width: 100%;
+    border: 1px solid light blue;
+    border-radius: 20px;
+    height: 100%;
+
+
+    button {
+        
+    }
+`
